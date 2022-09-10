c9c426ac311683e5b1dd76417bf386fc25ddd2a3
Narfe95 <Narfe95@users.noreply.github.com>
2022-09-10 16:16:19 +0200

    Changed oscparams default values

--- main.py
+++ main.py
@@ -8,43 +8,38 @@ from dataclasses import dataclass
 from typing import Union
 
 from pythonosc.dispatcher import Dispatcher
-from pythonosc.udp_client import SimpleUDPClient
 from pythonosc.osc_server import ThreadingOSCUDPServer
-
-IP = "127.0.0.1"
-RECEIVER_PORT = 9001
-SENDER_PORT = 9000
-BASE_ADDRESS = "/avatar/parameters"
+from pythonosc.udp_client import SimpleUDPClient
 
 
 @dataclass
 class OSCParams:
-    ip: str
-    receiver_port: int
-    sender_port: int
-    base_osc_address: str
+    ip: str = "127.0.0.1"
+    receiver_port: int = 9001
+    sender_port: int = 9000
+    base_osc_address: str = "/avatar/parameters"
 
 
 class OSC:
 
     def __init__(self, osc_params: OSCParams):
-        self.__osc_params = osc_params
-        self.__party_members = list()
+        self.osc_params = osc_params
+        self.party_members = list()
         dispatcher = Dispatcher()
-        dispatcher.map(f"{self.__osc_params.base_osc_address}/ExampleAction", self.__example_action)
-        self.server = ThreadingOSCUDPServer((self.__osc_params.ip, self.__osc_params.receiver_port), dispatcher)
+        dispatcher.map(f"{self.osc_params.base_osc_address}/ExampleAction", self.example_action)
+        self.server = ThreadingOSCUDPServer((self.osc_params.ip, self.osc_params.receiver_port), dispatcher)
 
-    def __example_action(self, _, *args):
+    def example_action(self, _, *args):
         pass
 
-    def __send_message(self, parameter: str, value: Union[int, float, bytes, str, bool, tuple, list]) -> None:
-        __client = SimpleUDPClient(self.__osc_params.ip, self.__osc_params.sender_port)
-        __client.send_message(f"{self.__osc_params.base_osc_address}/{parameter}", value)
-        print(f"Sent message to '{self.__osc_params.base_osc_address}/{parameter}' with a value of '{value}'")
+    def send_message(self, parameter: str, value: Union[int, float, bytes, str, bool, tuple, list]) -> None:
+        client = SimpleUDPClient(self.osc_params.ip, self.osc_params.sender_port)
+        client.send_message(f"{self.osc_params.base_osc_address}/{parameter}", value)
+        print(f"Sent message to '{self.osc_params.base_osc_address}/{parameter}' with a value of '{value}'")
 
 
 if __name__ == "__main__":
-    osc = OSC(OSCParams(IP, RECEIVER_PORT, SENDER_PORT, BASE_ADDRESS))
+    osc = OSC(OSCParams())
     print(f"Starting server")
     try:
         osc.server.serve_forever()
